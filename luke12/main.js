0fd6f5534da1bc63511e5e8c15911e0dd4c544e6
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2021-12-15 18:00:40 +0100

    luke12

--- luke12/main.js
+++ luke12/main.js
@@ -2,23 +2,12 @@ var lineReader = require("readline").createInterface({
   input: require("fs").createReadStream("task.txt"),
 });
 
-let prevDept = 0;
-let numberOfLines = 1;
-let prevType = "";
-let catRemoved = 0;
-let giftDept = 0;
-let prevGiftDept = 0;
-let catRemoveCounter = 0;
-let totalCatCounter = 0;
+let index = 0;
+let previosLines = [];
+let validCategoriesIndex = [];
 
 lineReader.on("line", (line) => {
   let lineDept = 0;
-  let text = line;
-
-  if (numberOfLines > 100) {
-    return;
-  }
-
   let type = "";
   let counter = 0;
   while (type === "") {
@@ -32,33 +21,22 @@ lineReader.on("line", (line) => {
     counter++;
   }
 
-  if (type == "G" && prevType == "K" && lineDept === prevDept) {
-    //catRemoved = catRemoveCounter - (prevDept - lineDept);
-    catRemoved += 1;
-    catRemoveCounter = 0;
-  } else if (type == "G" && prevType == "K" && lineDept < prevDept) {
-    catRemoved += catRemoveCounter;
-  } else if (type == "G") {
-    catRemoveCounter = 0;
-  } else if (type == "K" && lineDept <= prevDept && prevType == "K") {
-    catRemoved += catRemoveCounter;
-    catRemoveCounter = 0;
+  if (type === "G" && lineDept > 0) {
+    let curDept = lineDept;
+    for (let i = previosLines.length - 1; i >= 0; i--) {
+      if (previosLines[i].dept === curDept - 1) {
+        curDept = previosLines[i].dept;
+        validCategoriesIndex.indexOf(previosLines[i].idx) === -1
+          ? validCategoriesIndex.push(previosLines[i].idx)
+          : null;
+      }
+    }
   }
 
-  if (type == "K") {
-    catRemoveCounter++;
-    totalCatCounter++;
-  }
-
-  prevDept = lineDept;
-  prevType = type;
-  numberOfLines++;
-  text += " (" + catRemoved + ")";
-  console.log(text);
+  previosLines.push({ line: line, dept: lineDept, idx: index });
+  index++;
 });
 
 lineReader.on("close", () => {
-  console.log("categories removed: " + catRemoved);
-  console.log("total categories: " + totalCatCounter);
-  console.log("answer: " + (totalCatCounter - catRemoved));
+  console.log("answer: " + validCategoriesIndex.length);
 });
